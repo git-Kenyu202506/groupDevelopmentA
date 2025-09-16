5dbebb8d3ce77042303f86871ee27f87f680379c
Risa Kawai <>
2025-09-16 21:01:03 +0900

    DummyLoginControllerに変更

--- src/main/java/com/example/demo/controller/DummyLoginController.java
+++ src/main/java/com/example/demo/controller/DummyLoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class LoginController {
+public class DummyLoginController {
 
     @GetMapping("/login")
     public String loginPage() {
